4b3682624c6bcea40e203dc8c6add9d9225b44b2
xjr <619854442@qq.com>
2017-05-10 22:27:15 +0800

    第四天

---
+++ v-loader-demo/App.vue
@@ -0,0 +1,20 @@
+<template>
+   <h1>welcome</h1>
+   <h2>{{msg}}</h2>
+</template>
+<script>
+
+export default{
+	data(){
+	  retrun{
+	  msg:'welcome vue'
+	  }
+	}
+}
+</script>
+<style>
+body{
+	background:red;
+}
+
+</style>
---
+++ v-loader-demo/main.js
@@ -0,0 +1,8 @@
+import App from './App.vue'
+import Vue from 'vue'
+new Vue({
+	el:'body',
+	components:{
+		app:App,
+	}
+});
